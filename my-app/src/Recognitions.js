b092e6f4e54446a66c5a21478b50b1a9c61eb55d
Ashley Elder <ashley.elder@techtonic.com>
2018-10-30 15:20:43 -0600

    recognitions class

---
+++ my-app/src/Recognitions.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+//logout Component
+class Recognitions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterResponses: [],
+    };
+  }
+  //renders everything after the navbar
+  render () {
+    return (
+      <h1>testing Recognitions</h1>
+    );
+  }
+}
+
+export default Recognitions;
