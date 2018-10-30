c923e46eb78a769a37ca812bea01a54d88284336
Merry Degaga <merry.degaga@techtonic.com>
2018-10-30 10:46:33 -0600

    footer added to index.html

--- my-app/src/RR.js
+++ my-app/src/RR.js
@@ -78,12 +78,6 @@ const RR = () => {  return ( <body class="flex-container">
         </div>
       </div>
     </div>
-
-      <footer class="text-center my-4 pb-4">
-        <span class="text-muted">BeeRecognized &#x24B8; 2018
-        </span>
-      </footer>
-
     <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
