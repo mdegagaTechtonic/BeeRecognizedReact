b1815811557d739e3a552b1a29ce85d04cd12c47
Jason Dang <jason.dang@techtonic.com>
2018-10-31 09:47:25 -0600

    added comments to recognitions.js

--- my-app/src/Recognitions.js
+++ my-app/src/Recognitions.js
@@ -7,12 +7,15 @@ import GetUser from './getUser.js';
 class Recognitions extends Component {
   constructor(props) {
     super(props);
-    this.state = {page:'', beginDate: new Date(), endDate: new Date(), userSelected: ''};
+    this.state = {page:'', filteredArr:[], beginDate: new Date(), endDate: new Date(), userSelected: ''};
+    //state filteredArr will initially be set to mockdb
   }
 
   onChangeBeginDate = beginDate => this.setState({beginDate: beginDate});
   onChangeEndDate = endDate => this.setState({endDate: endDate});
   onChangeGetUser = (e) => this.setState({userSelected: e.target.value.toString()});
+
+  //this is where filter logic will happen and filteredArr state will be set
   onFilterButton = (e) => {
     alert(this.state.userSelected);
     alert(this.state.beginDate);
@@ -20,13 +23,18 @@ class Recognitions extends Component {
   }
 
   render() {
+
+    // static label for username input datalist
     let label = '';
     if(this.props.page=='RR') label = 'Received from';
-    else label = 'Sent to'
-    var datalist=['test','bill','egor'];
+    else label = 'Sent to';
+
+    var datalist=['test','bill','egor'];  // test data will be replace with data from mockdb
     require('./calendar.gif');
     let calendarIco = './calendar.gif';
       return (
+
+        // rendering of filter component
         <div className='d-flex flex-row flex-wrap'>
           <div className='p-2'>
             <label className='mr-2'>{label}</label>
@@ -42,6 +50,8 @@ class Recognitions extends Component {
             <button className='btn-info' onClick={this.onFilterButton}>Filter</button>
           </div>
         </div>
+
+        //rendering of filtered recognitions by calling display component and passing down filteredArr
       );
   };
 };
