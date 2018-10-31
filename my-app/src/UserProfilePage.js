bfc7582600bc1cb2a6637a3b4bb2eace6e961f4a
Erik Hoy <>
2018-10-31 13:56:09 -0600

    added heading to sidebar

--- my-app/src/UserProfilePage.js
+++ my-app/src/UserProfilePage.js
@@ -79,7 +79,14 @@ class UserProfilePage extends React.Component {
             </div>
           </div>
           <div id="sidebar" className="bg-white h-25 d-inline-block col-12 col-md-6 col-lg-4 rounded border border-dark mb-2 p-4">
-            <DisplayList recognitions={recognitionsArray} />
+            <nav className="flex-item row-flex navbar-fixed-right">
+              <div className="list-group-item list-group-item-action flex-column align-items-start">
+                <div className="d-flex w-100 justify-content-between">
+                  <h3 className="mb-1">Recognition Received</h3>
+                </div>
+              </div>
+              <DisplayList recognitions={recognitionsArray} />
+            </nav>
           </div>
         </div>
       </div>
