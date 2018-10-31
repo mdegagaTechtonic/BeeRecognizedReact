68b0fa790d38abde24f3fd4b1696a7c278b541e0
Merry Degaga <merry.degaga@techtonic.com>
2018-10-31 13:21:44 -0600

    added logic for logging in and logging out

--- my-app/src/login.js
+++ my-app/src/login.js
@@ -1,29 +1,23 @@
 import React from 'react';
 
-const login = () => {
-  return (  <html lang="en" dir="ltr">
-  <body class="flex-container content">
-      <main class="flex-grow">
-        <div class="container-fluid">
-          <div class="row justify-content-around my-auto mx-1">
-            <div class="col-10 col-sm-7 col-md-7 col-lg-5 col-xl-4 bg-white border border-dark rounded p-4 mt-5">
-              <h1 class="text-center">BeeRecognized</h1>
-              <div class="img text-center">
+class login extends React.Component {
+  render () {return (
+
+          <div className="row justify-content-around my-auto mx-1">
+            <div className="col-10 col-sm-7 col-md-7 col-lg-5 col-xl-4 bg-white border border-dark rounded p-4 mt-5">
+              <h1 className="text-center">BeeRecognized</h1>
+              <div className="img text-center">
                 <img id="img" src="images/bee.png" width="50"/>
-              </div>
-              <div class="text-center my-5">
-                <a href="#"><img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" id ="slack"/></a>
-              </div>
-              <div id="link" class="text-center mt-0">
-                <a href="https://slack.com">If you don't have Slack account, click here.</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    <script src="js/login.js"></script>
-  </body>
-</html>);
+                            <div class="text-center my-5">
+                              <a href=""><img onClick={this.props.action} src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" id ="slack"/></a>
+                            </div>
+                            <div id="link" class="text-center mt-0">
+                              <a href="https://slack.com">If you don't have Slack account, click here.</a>
+                            </div>
+                          </div>
+                        </div>
+            </div>);
 };
+}
 
 export default login;
