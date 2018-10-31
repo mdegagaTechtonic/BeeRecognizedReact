aae26cb73fc370ec483b7c2de284fe2e09858999
Erik Hoy <>
2018-10-31 11:53:14 -0600

    added array to Recognitions

--- my-app/src/Recognitions.js
+++ my-app/src/Recognitions.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Filter from './filter.js';
-
+import DisplayList from './DisplayList.js';
 import DatePicker from 'react-date-picker';
 import GetUser from './getUser.js';
 
@@ -21,26 +21,37 @@ class Recognitions extends Component {
 
   render() {
     let label = '';
-    if(this.props.page=='RR') label = 'Received from';
-    else label = 'Sent to'
+    var recognitionsArray = [];
     var datalist=['test','bill','egor'];
+    var recognitionSentArray = [{ name: 'Ashley', avatar: 'ashley.elder.png', date: '10/31/18', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id arcu risus. Praesent consequat mollis dolor, eu tristique neque scelerisque egestas.' }, { name: 'Brett', avatar: 'BrettGoers.png', date: '10/29/18', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id arcu risus. Praesent consequat mollis dolor, eu tristique neque scelerisque egestas.' }];
+    var recognitionReceivedArray = [{ name: 'Erik', avatar: 'erikhoy.png', date: '10/25/18', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id arcu risus. Praesent consequat mollis dolor, eu tristique neque scelerisque egestas.' }, { name: 'Egor', avatar: 'Egor.png', date: '10/23/18', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id arcu risus. Praesent consequat mollis dolor, eu tristique neque scelerisque egestas.' }];
     require('./calendar.gif');
     let calendarIco = './calendar.gif';
+    if (this.props.page=='RR') {
+      label = 'Received from';
+      recognitionsArray = recognitionReceivedArray;
+    } else {
+      label = 'Sent to';
+      recognitionsArray = recognitionSentArray;
+    }
       return (
-        <div className='d-flex flex-row flex-wrap'>
-          <div className='p-2'>
-            <label className='mr-2'>{label}</label>
-            <GetUser listusers={datalist} onChange={this.onChangeGetUser}/>
+        <div>
+          <div className='d-flex flex-row flex-wrap'>
+            <div className='p-2'>
+              <label className='mr-2'>{label}</label>
+              <GetUser listusers={datalist} onChange={this.onChangeGetUser}/>
+            </div>
+            <div className='p-2'>
+              From
+              <DatePicker className='p-2' onChange={this.onChangeBeginDate} value={this.state.beginDate} clearIcon={null} calendarIcon={calendarIco}/>
+              To
+              <DatePicker className='p-2' onChange={this.onChangeEndDate} value={this.state.endDate} clearIcon={null} />
+            </div>
+            <div className='p-3'>
+              <button className='btn-info' onClick={this.onFilterButton}>Filter</button>
+            </div>
           </div>
-          <div className='p-2'>
-            From
-            <DatePicker className='p-2' onChange={this.onChangeBeginDate} value={this.state.beginDate} clearIcon={null} calendarIcon={calendarIco}/>
-            To
-            <DatePicker className='p-2' onChange={this.onChangeEndDate} value={this.state.endDate} clearIcon={null} />
-          </div>
-          <div className='p-3'>
-            <button className='btn-info' onClick={this.onFilterButton}>Filter</button>
-          </div>
+          <DisplayList recognitions={recognitionsArray} />
         </div>
       );
   };
