5d5df61ec171cd34be45dca148ac76044dd6bff9
Merry Degaga <merry.degaga@techtonic.com>
2018-11-01 11:38:32 -0600

    fixed form, so that it clears after submission

--- my-app/src/UserProfilePage.js
+++ my-app/src/UserProfilePage.js
@@ -17,20 +17,25 @@ class UserProfilePage extends React.Component {
       numberOfBeesReceived: '10',
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearForm = this.clearForm.bind(this);
+
   }
 
   handleSubmit (event) {
     //grab form info
-    let serialize = require('form-serialize');
-    let form = document.querySelector('form');
-    form = serialize(form, { hash: true });
-    let r = new Recognition(`avatars/${this.props.currUser}`,`avatars/${form.receiver}`, this.props.currUser, form.receiver, this.state.numberOfBeesToGive, new Date(Date.now()).toDateString(), form.message);
+    let r = new Recognition(`avatars/${this.props.currUser}`,`avatars/${this._receiver.value}`, this.props.currUser, this._receiver.value, this.state.numberOfBeesToGive, new Date(Date.now()).toDateString(), this._message.value);
     //grab local localStorage
     const db = JSON.parse(localStorage.getItem('db'));
     //maintains the chronological order of our db
     db.unshift(r);
     localStorage.setItem('db',(JSON.stringify(db)));
+    this.clearForm();
     event.preventDefault();
+  }
+
+  clearForm () {
+    this._receiver.value = '';
+    this._message.value = '';
   }
 
 
@@ -79,15 +84,15 @@ class UserProfilePage extends React.Component {
                 <p className="alert alert-danger d-none" role="alert" id="danger">Oh no, you have no bees left to give! They are on vacation, come back later.</p>
               </div>
               {/* <!-- recognition form --> */}
-              <form action="" method="" autocomplete="off" ref={el => (this.form = el)} onSubmit={this.handleSubmit}>
+              <form action="" method="" autocomplete="off" onSubmit={this.handleSubmit}>
                 <div className="form-group input-group-lg">
                   <label for="username">Select User to Recognize</label><br/>
-                  <input type="text" list="br/owsers" className="form-control rounded border border-secondary" placeholder="Username" name="receiver"/>
+                  <input type="text" list="br/owsers" className="form-control rounded border border-secondary" placeholder="Username" name="receiver" ref={input => this._receiver = input}/>
                     <datalist id="br/owsers"></datalist>
                 </div>
                 <div className="form-group input-group-lg">
                   <label for="message">Message</label><br/>
-                  <textarea name="message" placeholder="Include a message here" className="form-control border border-secondary" rows="3"></textarea>
+                  <textarea name="message" placeholder="Include a message here" className="form-control border border-secondary" rows="3" ref={textarea => this._message = textarea}></textarea>
                 </div>
                 <div id="hiddenBeesToGive" className="form-group">
                   <input type="hidden" name="beesToGive"/>
