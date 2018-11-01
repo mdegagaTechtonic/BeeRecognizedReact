83481b52696add0263b6c3a0b0c1858eb2973504
Jason Dang <jason.dang@techtonic.com>
2018-11-01 15:26:33 -0600

    modify beesreceived.js

--- my-app/src/Bees/BeesReceived.js
+++ my-app/src/Bees/BeesReceived.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 export const BeesReceived = (props) => {
+
+let styleClass = '';
+ if(props.page == 'RR' || props.page == 'UP') styleClass = 'badge badge-pill badge-success align-self-center';
+ else styleClass = 'badge badge-pill badge-dark align-self-center';
   return (
-    <span className='badge badge-pill badge-success align-self-center'>Total:&nbsp;{props.bees}</span>
+    <span className={styleClass}>Total:&nbsp;{props.bees} </span>
   );
 };
 
