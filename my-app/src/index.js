d0b26e1b3c75cca05be23f7eb025f56c2d8adfff
Jason Dang <jason.dang@techtonic.com>
2018-10-31 19:13:11 -0600

    clean up index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Recognitions from './Recognitions.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-//testing for jason
-//ReactDOM.render(<Recognitions page={'RR'} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
