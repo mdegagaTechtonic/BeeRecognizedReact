24893034233b3bac205ebbee27199885faf58dc6
Ashley Elder <ashley.elder@techtonic.com>
2018-11-02 14:13:54 -0600

    refactor utils

--- my-app/src/utils.js
+++ my-app/src/utils.js
@@ -1,6 +1,6 @@
 export function getAllRecognitionReceived(db, username) {
-  var receivedRecognition = [];
-  for (var i = 0; i < db.length; i++) {
+  const receivedRecognition = [];
+  for (let i = 0; i < db.length; i++) {
     if (db[i].receiver.toLowerCase() === username.toLowerCase()) {
       receivedRecognition.push(db[i]);
     }
@@ -9,9 +9,9 @@ export function getAllRecognitionReceived(db, username) {
 }
 
 export function getRecentRecognition(db, username) {
-  var received = getAllRecognitionReceived(db, username);
+  const received = getAllRecognitionReceived(db, username);
   if (received.length > 5) {
-    var recentFive = received.slice(0, 5);
+    const recentFive = received.slice(0, 5);
     return recentFive;
   }
   if (received.length <= 5) {
@@ -20,8 +20,8 @@ export function getRecentRecognition(db, username) {
 };
 
 export function getAllRecognitionSent(db, username) {
-  var sentRecognition = [];
-  for (var i = 0; i < db.length; i++) {
+  const sentRecognition = [];
+  for (let i = 0; i < db.length; i++) {
     if (db[i].sender.toLowerCase() === username.toLowerCase()) {
       sentRecognition.push(db[i]);
     }
