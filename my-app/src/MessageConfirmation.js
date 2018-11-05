9b043c0db3b1de1aa9f5c0de5e903f8e5f6e3d04
Merry Degaga <merry.degaga@techtonic.com>
2018-11-05 11:54:27 -0700

    added in logic for sending in recogintion, user can send a message as long as they have sufficienti number of bees to give, entered in a user to send it to, and a message, appropriate alerts appear depending on what is missing

--- my-app/src/MessageConfirmation.js
+++ my-app/src/MessageConfirmation.js
@@ -1,9 +1,39 @@
 import React from 'react';
+var classNames = require( 'classnames/bind' );
 
-export default function MessageConfirmation (numberOfBeesToGive) {
-  if(numberOfBeesToGive > 0) {
-    return (<p className="alert alert-success" role="alert" id="success">Thank you. Your recognition was sent.</p>);
-  } else{
-    return (<p className="alert alert-danger" role="alert" id="danger">Oh no, you have no bees left to give! They are on vacation, come back later.</p>);
+export default function MessageConfirmation (numberOfBeesToGive, receiver, message) {
+  const styles = {
+    'alert': true,
+    'alert-danger': true,
+    'alert-success': true,
+  };
+
+  let text = '';
+  
+  let id = 'danger';
+
+  switch(true) {
+    case numberOfBeesToGive <= 0:
+    text = 'Oh no, you have no bees left to give! They are on vacation, come back later.';
+    styles['alert-success'] = false;
+      break;
+    case !receiver && !message:
+    text = 'Please enter in a person and message before submitting.';
+    styles['alert-success'] = false;
+      break;
+    case !receiver:
+    text = 'Please enter in a person you would like to send recognition to.';
+    styles['alert-success'] = false;
+      break;
+    case !message:
+    text = 'Please provide a message.';
+    styles['alert-success'] = false;
+      break;
+    default:
+      text = 'Thank you. Your recognition was sent.';
+      id = 'success';
+      styles['alert-danger'] = false;
+      break;
   }
+  return <p className={classNames(styles)} role="alert" id={id}>{text}</p>;
 }
